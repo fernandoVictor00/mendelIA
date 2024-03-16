8fb194bc214021f229826a7eeb75cf4c6afaed2f
naah16 <naarah.oliveira@gmail.com>
2024-03-16 14:10:27 -0300

    removing material ui

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 
 @Component({
