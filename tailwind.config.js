c94282ee8ddeb5ae6fbacc0f4288e387a143cff6
naah16 <naarah.oliveira@gmail.com>
2024-03-16 15:39:58 -0300

    adjuste style form

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'biofy-blue': '#008FBB',
+        'biofy-blue-more': '#00A6D9',
+        'biofy-cyan': '#00AD7F',
+        'biofy-cyan-more': '#00C99A',
+        'biofy-blue-dark': '#002D42',
+        'biofy-blue-dark-more': '#001F2C',
+      }
+    },
   },
   plugins: [],
 }
