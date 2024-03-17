e6a42b78c6d1e82c09f71559b82fad93a8e5f795
fernando <fernando00sjp@hotmail.com>
2024-03-17 13:25:23 -0300

    adjust-routing

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, AbstractControl  } from '@angular/forms';
+import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { FileUploadService } from '../../services/oracle/oracle.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,25 +15,45 @@ export class HomeComponent implements OnInit {
   selectedFile: File = null!;
   selectedFileName: string = '';
 
-  constructor(private _fb: FormBuilder, oracleService: FileUploadService) {
+  constructor(
+    private _fb: FormBuilder,
+    oracleService: FileUploadService,
+    private router: Router
+  ) {
     this.oracleService = oracleService;
     this.requestForm = this._fb.group({
+      nome: [''],
       dna: ['', [Validators.required, this.dnaValidator]],
-      uploadFile: []
+      uploadFile: [],
     });
   }
 
   ngOnInit() {}
 
   async createRequest() {
+    if (!this.requestForm.value.nome) {
+      alert('A sua amostra precisa de um nome');
+      return;
+    }
     if (this.selectedFile) {
       this.isLoading = true;
       this.oracleService.uploadFileFirebase(this.selectedFile).then((url) => {
         this.isLoading = false;
         this.selectedFileName = 'Selecione o arquivo';
         this.selectedFile = null!;
-        console.log('URL do arquivo:', url);
+        const name = this.requestForm.value.nome
+          ? this.requestForm.value.nome
+          : '';
+        this.showDashboard(true, url, '', name);
       });
+    } else if (this.requestForm.value.dna) {
+      const name = this.requestForm.value.nome
+        ? this.requestForm.value.nome
+        : '';
+      const dna = this.requestForm.value.dna ? this.requestForm.value.dna : '';
+      this.showDashboard(true, '', dna, name);
+    } else {
+      alert('Por favor, selecione um arquivo ou insira uma sequência de DNA');
     }
   }
 
@@ -50,8 +71,23 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  showDashboard(
+    isUpload: boolean = false,
+    urlImage: string = '',
+    dna: string = '',
+    name: string = ''
+  ) {
+    let url = 'http://localhost:4200/dashboard';
+    url += `?isUpload=${isUpload}&urlImage=${encodeURIComponent(
+      urlImage
+    )}&dna=${encodeURIComponent(dna)}&name=${encodeURIComponent(name)}`;
+    window.open(url, '_blank');
+  }
+
   dnaValidator(control: AbstractControl): { [key: string]: boolean } | null {
-    const value = control.value ? control.value.toUpperCase().replace(/[^ACGTN]/g, '') : '';
+    const value = control.value
+      ? control.value.toUpperCase().replace(/[^ACGT]/g, '')
+      : '';
     if (value !== control.value) {
       control.setValue(value);
     }
